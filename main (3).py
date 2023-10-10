75ce3e03519cb4cb29d2ff60f903f0e311e086a1
Pavithrapavithra2023 <144545266+Pavithrapavithra2023@users.noreply.github.com>
2023-10-10 14:14:10 +0530

    Update main (3).py

--- main (3).py
+++ main (3).py
@@ -1,15 +1,11 @@
-def is_leap_year(year):
-    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-        return True
-    else:
-        return False
 
-# Input year from the user
-try:
-    year = int(input("Enter a year: "))
-    if is_leap_year(year):
-        print(f"{year} is a leap year.")
-    else:
-        print(f"{year} is not a leap year.")
-except ValueError:
-    print("Invalid input. Please enter a valid year.")
+# Factorial
+def fact_rec(n):
+  if n==0 or n==1:
+     return 1
+  else:
+     return n*fact_rec(n-1)
+number = int(input("Enter a value :"))
+res = fact_rec(number)
+
+print("The factorial of {} is {} .". format (number,res))
